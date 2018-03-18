1c1b5764d80048bc76279a43190095e0f8033047
twobit <s8572327@gmail.com>
2018-03-18 20:30:36 +0100

    prettier map rendering

--- clientworld.lua
+++ clientworld.lua
@@ -21,6 +21,8 @@ function ClientWorld:init()
 		end
 	end
 end
+
+
 function ClientWorld:spawn_blood(x, y)
 	local a = math.random() * 2 * math.pi
 	local s = math.random() * 3 + 1
@@ -39,6 +41,8 @@ function ClientWorld:spawn_blood(x, y)
 		shade  = 0.6 + math.random() * 0.4,
 	})
 end
+
+
 function ClientWorld:decode_state(state)
 
 	local n = state:gmatch("([^ ]+)")
@@ -145,6 +149,8 @@ function ClientWorld:decode_state(state)
 	end
 	self.event_tick = event_tick
 end
+
+
 function ClientWorld:process_event(e)
 	if e[1] == "b" then
 		-- bullet particle
@@ -164,6 +170,8 @@ function ClientWorld:process_event(e)
 		end
 	end
 end
+
+
 function ClientWorld:update()
 	self.tick = self.tick + 1
 
@@ -221,6 +229,124 @@ function ClientWorld:update()
 
 	end
 end
+
+
+function ClientWorld:draw_map(cam)
+	local lava_poly = { TILE_SIZE, TILE_SIZE, 0, TILE_SIZE }
+	local lava_poly2 = { TILE_SIZE, TILE_SIZE, 0, TILE_SIZE }
+	for i = 0, TILE_SIZE, TILE_SIZE / 4 do
+		lava_poly[#lava_poly + 1] = i
+		lava_poly[#lava_poly + 1] = math.sin(i / TILE_SIZE * 2 * math.pi + self.tick * 0.1) * 2
+	end
+	for i = 0, TILE_SIZE, TILE_SIZE / 3 do
+		lava_poly2[#lava_poly2 + 1] = i
+		lava_poly2[#lava_poly2 + 1] = math.sin(i / TILE_SIZE * 2 * math.pi - self.tick * 0.07) * 2
+	end
+
+	local function X(x, y)
+		return x + math.sin(x * 7777 + y * 9999) * 2
+	end
+	local function Y(x, y)
+		return y + math.sin(x * 5555 + y * 3333) * 2
+	end
+
+	local x1 = math.floor((cam.x - W / 2) / TILE_SIZE)
+	local x2 = math.floor((cam.x + W / 2) / TILE_SIZE)
+	local y1 = math.floor((cam.y - H / 2) / TILE_SIZE)
+	local y2 = math.floor((cam.y + H / 2) / TILE_SIZE)
+
+
+	-- grounding
+	G.setColor(80, 50, 100)
+	for y = y1, y2 do
+		for x = x1, x2 do
+			local t = World:tile_at(x, y)
+			if t == "0" then
+				G.rectangle("fill", x * 16, y * 16, 16, 16)
+			end
+		end
+	end
+
+	-- wiggly stones
+	for y = y1, y2 do
+		for x = x1, x2 do
+			local t = World:tile_at(x, y)
+			if t == "0" then
+				local d = ((x * 12.341 + y * 31.421) ^ 1.2) * 41 % 30
+				if d < 1 then
+				elseif d < 2 then
+				elseif d % 4.2 > 4 then
+				else
+					G.setColor(110, 80, 120)
+					local x1 = x * 16
+					local x2 = x * 16 + 16
+					local y1 = y * 16
+					local y2 = y * 16 + 16
+					local poly = {
+						X(x1, y1), Y(x1, y1),
+						X(x2, y1), Y(x2, y1),
+						X(x2, y2), Y(x2, y2),
+						X(x1, y2), Y(x1, y2),
+					}
+					G.polygon("fill", poly)
+				end
+			end
+		end
+	end
+
+	-- special stones
+	for y = y1, y2 do
+		for x = x1, x2 do
+			local t = World:tile_at(x, y)
+			if t == "0" then
+				local d = ((x * 12.341 + y * 31.421) ^ 1.2) * 41 % 30
+				if d < 1 then
+
+					G.setColor(100, 70, 110)
+					G.rectangle("fill", x * 16, y * 16, 16, 16, r)
+					G.setColor(60, 80, 90)
+					G.rectangle("fill", x * 16 + 2, y * 16 + 3, 7, 6)
+
+				elseif d < 2 then
+					-- screws
+					G.setColor(100, 80, 100)
+					G.rectangle("fill", x * 16, y * 16, 16, 16)
+					G.setColor(60, 60, 80)
+					G.rectangle("fill", x * 16 + 1, y * 16 + 1, 2, 2)
+					G.rectangle("fill", x * 16 + 1, y * 16 + 13, 2, 2)
+					G.rectangle("fill", x * 16 + 13, y * 16 + 1, 2, 2)
+					G.rectangle("fill", x * 16 + 13, y * 16 + 13, 2, 2)
+
+				elseif d % 4.2 > 4 then
+					-- rounded box
+					G.setColor(110, 100, 120)
+					G.rectangle("fill", x * 16, y * 16, 16, 16, 3)
+				end
+
+			elseif t == "^" then
+				G.setColor(50, 30, 0)
+				G.rectangle("fill", x * 16, y * 16, 16, 4)
+				G.setColor(130, 80, 50)
+				G.rectangle("line", x * 16 + 0.5, y * 16 + 0.5, 7, 3)
+				G.rectangle("line", x * 16 + 0.5 + 8, y * 16 + 0.5, 7, 3)
+
+			elseif t == "L" then
+				-- lava
+				G.push()
+				G.translate(x * 16, y * 16)
+				G.setColor(100, 0, 0)
+				G.polygon("fill", lava_poly2)
+				G.setColor(130, 0, 0)
+				G.polygon("fill", lava_poly)
+				G.pop()
+			end
+		end
+	end
+
+
+end
+
+
 function ClientWorld:draw()
 	G.push()
 	local cam = World.spawning_points[1]
@@ -251,65 +377,8 @@ function ClientWorld:draw()
 
 
 	-- map
-	do
-		local lava_poly = { TILE_SIZE, TILE_SIZE, 0, TILE_SIZE }
-		local lava_poly2 = { TILE_SIZE, TILE_SIZE, 0, TILE_SIZE }
-		for i = 0, TILE_SIZE, TILE_SIZE / 4 do
-			lava_poly[#lava_poly + 1] = i
-			lava_poly[#lava_poly + 1] = math.sin(i / TILE_SIZE * 2 * math.pi + self.tick * 0.1) * 2
-		end
-		for i = 0, TILE_SIZE, TILE_SIZE / 3 do
-			lava_poly2[#lava_poly2 + 1] = i
-			lava_poly2[#lava_poly2 + 1] = math.sin(i / TILE_SIZE * 2 * math.pi - self.tick * 0.07) * 2
-		end
+	self:draw_map(cam)
 
-		local x1 = math.floor((cam.x - W / 2) / TILE_SIZE)
-		local x2 = math.floor((cam.x + W / 2) / TILE_SIZE)
-		local y1 = math.floor((cam.y - H / 2) / TILE_SIZE)
-		local y2 = math.floor((cam.y + H / 2) / TILE_SIZE)
-		for y = y1, y2 do
-			for x = x1, x2 do
-				local t = World:tile_at(x, y)
-				if t == "0" then
-					local d = ((x * 12.341 + y * 31.421) ^ 1.2) * 41 % 30
-					if d < 1 then
-						G.setColor(110, 80, 120)
-						G.rectangle("fill", x * 16, y * 16, 16, 16, r)
-						G.setColor(60, 80, 90)
-						G.rectangle("fill", x * 16 + 2, y * 16 + 3, 7, 6)
-					elseif d < 2 then
-						G.setColor(100, 80, 100)
-						G.rectangle("fill", x * 16, y * 16, 16, 16)
-						G.setColor(60, 60, 80)
-						G.rectangle("fill", x * 16 + 1, y * 16 + 1, 2, 2)
-						G.rectangle("fill", x * 16 + 1, y * 16 + 13, 2, 2)
-						G.rectangle("fill", x * 16 + 13, y * 16 + 1, 2, 2)
-						G.rectangle("fill", x * 16 + 13, y * 16 + 13, 2, 2)
-					elseif d % 4.2 > 4 then
-						G.setColor(110, 100, 120)
-						G.rectangle("fill", x * 16, y * 16, 16, 16, 3)
-					else
-						G.setColor(110, 80, 120)
-						G.rectangle("fill", x * 16, y * 16, 16, 16)
-					end
-				elseif t == "^" then
-					G.setColor(50, 30, 0)
-					G.rectangle("fill", x * 16, y * 16, 16, 4)
-					G.setColor(130, 80, 50)
-					G.rectangle("line", x * 16 + 0.5, y * 16 + 0.5, 7, 3)
-					G.rectangle("line", x * 16 + 0.5 + 8, y * 16 + 0.5, 7, 3)
-				elseif t == "L" then
-					G.push()
-					G.translate(x * 16, y * 16)
-					G.setColor(100, 0, 0)
-					G.polygon("fill", lava_poly2)
-					G.setColor(130, 0, 0)
-					G.polygon("fill", lava_poly)
-					G.pop()
-				end
-			end
-		end
-	end
 
 	-- items
 	for _, item in ipairs(self.items) do
@@ -422,4 +491,3 @@ function ClientWorld:draw()
 		G.pop()
 	end
 end
-
